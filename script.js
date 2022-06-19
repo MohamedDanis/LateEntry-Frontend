7faad53203c5f9a08d23f7e718f7a21a9110028c
MohamedDanis <danishmanu17@gmail.com>
2022-06-19 19:57:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        
+    fontFamily: {
+      monda: ['Monda', 'sans-serif'],
+    },
+      extend: {
+        colors: {
+          grape: '#29192B',
+          secgray: '#6E8384',
+          terwhite: '#FAFEFF'
+        }
+      }
+    }
+  }
